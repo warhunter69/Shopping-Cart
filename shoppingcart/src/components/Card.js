e56b24bf16935a26767c11062b4fd297b74410c9
ahmed <warhunter69@gmail.com>
2022-01-11 15:23:58 +0200

    addtoCart

--- shoppingcart/src/components/Card.js
+++ shoppingcart/src/components/Card.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import '../styles/Card.css'
 const Card = (props) =>{
     const [hover, setHover] = useState();
-
+    const [qnty,setQnty] = useState(1);
     const handleMouseIn = () => {
       setHover(true);
     };
@@ -10,7 +10,11 @@ const Card = (props) =>{
     const handleMouseOut = () => {
       setHover(false);
     };
-  
+    const handleChange = (event) =>{
+      const value = parseInt(event.target.value, 10) ;
+      setQnty(value);
+    }
+    
     return(
         <div className="card" onClick={props.clickCard} onMouseOver={handleMouseIn} onMouseOut={handleMouseOut}>
            
@@ -18,9 +22,9 @@ const Card = (props) =>{
                <span className='card_span' >{props.name}</span>
                <div className="input_group" hidden={!hover}>
                   <label>quantity
-                  <input type="number" className='input_qnty' min='1' name="quantity"/>
+                  <input type="number" className='input_qnty' min='1' name="quantity" onInput={handleChange}/>
                   </label>
-                  <button  className='btn' onClick={(e)=>props.addtoCart({name:props.name,img:props.src},e)}>
+                  <button  className='btn' onClick={(e)=>props.addtoCart({name:props.name,img:props.src,qnty:qnty},e)}>
                           add to cart
                   </button>
 
