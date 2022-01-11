15a374b2011484cf94863675a560d633477edc50
ahmed <warhunter69@gmail.com>
2022-01-11 18:35:38 +0200

    fix-scale-3

--- shoppingcart/src/components/Cartcard.js
+++ shoppingcart/src/components/Cartcard.js
@@ -1,4 +1,14 @@
-const Cartcard = () =>{
- return(<></>)
+import '../styles/Cartcard.css'
+const Cartcard = (props) => {
+    return (
+
+        <div className="card">
+
+            <img className='card_img' src={props.src} alt={props.name} />
+            <span className='card_span' >{props.name}</span>
+
+
+        </div>
+    )
 }
 export default Cartcard;
