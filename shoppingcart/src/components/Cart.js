caca6ccb70b446f13713646278e89f18572c5f59
ahmed <warhunter69@gmail.com>
2022-01-12 13:00:42 +0200

    1

--- shoppingcart/src/components/Cart.js
+++ shoppingcart/src/components/Cart.js
@@ -25,11 +25,15 @@ const Cart = (props) =>{
     }
  return(
      <>
+    <div class="Content">
+        <div>hello</div>
     <div id="mySidepanel" className="sidepanel" style={{width:width}} onClick={test}>
     {/*<a href="javascript:void(0)" class="closebtn" onClick={closeCart}>&times;</a>}*/}
     <button className="closebtn" onClick={closeCart}>&times;</button>
+    <div className="Sidecontainer">
     {props.Cartcards.map((card, index) => ( <Cartcard key={index} img={card.img} name ={card.name}/> ))}
-
+    </div>
+  </div>
   </div>
   
   <button className="cart_btn" onClick={openCart}><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="#ffffff" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="10" cy="20.5" r="1"/><circle cx="18" cy="20.5" r="1"/><path d="M2.5 2.5h3l2.7 12.4a2 2 0 0 0 2 1.6h7.7a2 2 0 0 0 2-1.6l1.6-8.4H7.1"/></svg></button>
